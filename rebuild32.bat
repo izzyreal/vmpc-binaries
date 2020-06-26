160780ddaff9b4fe55fba7357321739bc079339e
Izmar Verhage <izmaelverhage@gmail.com>
2020-06-26 19:45:37 +0200

    Add some more Windows build scripts

---
+++ rebuild32.bat
@@ -0,0 +1,10 @@
+cd C:\Users\Izmar\git\moduru
+CALL rebuild32.bat
+
+cd C:\Users\Izmar\git\ctoot
+CALL rebuild32.bat
+
+cd C:\Users\izmar\git\mpc
+CALL rebuild32.bat
+
+cd C:\Users\izmar\git\vmpc-binaries
