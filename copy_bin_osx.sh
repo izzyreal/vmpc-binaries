b0cc213c0ef23d9c1a81e847f41231cbddcd7821
Izmar Verhage <izmaelverhage@gmail.com>
2020-10-11 15:10:36 +0200

    Release 0.2.0 OSX

--- copy_bin_osx.sh
+++ copy_bin_osx.sh
@@ -2,9 +2,7 @@
 rm -rf osx/*
 cp -v -r ../vmpc-juce/Builds/MacOSX/build/Release/vmpc.app osx/
 cp -v -r ~/Library/Audio/Plug-Ins/Components/vmpc.component osx/
-cp -v -r ~/Library/Audio/Plug-Ins/VST/vmpc.vst osx/
 cp -v -r ~/Library/Audio/Plug-Ins/VST3/vmpc.vst3 osx/
 zip -v -r osx/vmpc.zip osx/vmpc.app
 zip -v -r osx/vmpc.component.zip osx/vmpc.component
-zip -v -r osx/vmpc.vst.zip osx/vmpc.vst
 zip -v -r osx/vmpc.vst3.zip osx/vmpc.vst3
