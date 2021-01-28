7e1d2cdd704dfd134724c156542f9c4f625ada39
Izmar Verhage <izmar.verhage@lunatech.nl>
2021-01-28 12:24:44 +0100

    Fix OSX copy script and release 0.3.0.0 for MacOS

--- copy_bin_osx.sh
+++ copy_bin_osx.sh
@@ -3,6 +3,7 @@ rm -rf osx/*
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/vmpc2000xl.app osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/vmpc2000xl.vst3 osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/AU/vmpc2000xl.component osx/
-zip -v -r -m osx/vmpc2000xl.zip osx/vmpc2000xl.app
-zip -v -r -m osx/vmpc2000xl.component.zip osx/vmpc2000xl.component
-zip -v -r -m osx/vmpc2000xl.vst3.zip osx/vmpc2000xl.vst3
+cd osx
+zip -v -r vmpc2000xl.zip vmpc2000xl.app
+zip -v -r -m vmpc2000xl.component.zip vmpc2000xl.component
+zip -v -r -m vmpc2000xl.vst3.zip vmpc2000xl.vst3
