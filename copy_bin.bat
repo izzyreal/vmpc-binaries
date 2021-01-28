d8aff7f2e68314b26e1430d9b42ad9f229e626e2
Izmar Verhage <izmaelverhage@gmail.com>
2021-01-28 12:05:07 +0100

    Update Windows copy script

--- copy_bin.bat
+++ copy_bin.bat
@@ -1,4 +1,4 @@
-xcopy /F /Y "..\vmpc-juce\Builds\VisualStudio2019-32bit\Win32\Release\Standalone Plugin\vmpc.exe" win32\
-xcopy /F /Y "..\vmpc-juce\Builds\VisualStudio2019-32bit\Win32\Release\VST3\vmpc.vst3" win32\
-xcopy /F /Y "..\vmpc-juce\Builds\VisualStudio2019-64bit\x64\Release LTO\Standalone Plugin\vmpc.exe" win64\
-xcopy /F /Y "..\vmpc-juce\Builds\VisualStudio2019-64bit\x64\Release LTO\VST3\vmpc.vst3" win64\
+xcopy /F /Y "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\vmpc2000xl.exe" win32\
+xcopy /F /Y /I "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\vmpc2000xl.vst3\" win32\
+xcopy /F /Y "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\vmpc2000xl.exe" win64\
+xcopy /F /Y /I "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\vmpc2000xl.vst3\" win64\
