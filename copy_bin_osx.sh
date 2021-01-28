d7a09bc00c1ec358e80b6e2a20e8430a30218926
Izmar Verhage <izmar.verhage@lunatech.nl>
2021-01-28 11:34:08 +0100

    Release 0.3.0.0 MacOS

--- copy_bin_osx.sh
+++ copy_bin_osx.sh
@@ -3,6 +3,6 @@ rm -rf osx/*
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/vmpc2000xl.app osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/vmpc2000xl.vst3 osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/AU/vmpc2000xl.component osx/
-zip -v -r osx/vmpc2000xl.zip osx/vmpc2000xl.app
-zip -v -r osx/vmpc2000xl.component.zip osx/vmpc2000xl.component
-zip -v -r osx/vmpc2000xl.vst3.zip osx/vmpc2000xl.vst3
+zip -v -r -m osx/vmpc2000xl.zip osx/vmpc2000xl.app
+zip -v -r -m osx/vmpc2000xl.component.zip osx/vmpc2000xl.component
+zip -v -r -m osx/vmpc2000xl.vst3.zip osx/vmpc2000xl.vst3
