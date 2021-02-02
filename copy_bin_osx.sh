53d59bef7451ef293eeeff7b9ad27cc503321022
Izmar Verhage <izmaelverhage@gmail.com>
2021-02-02 22:37:10 +0100

    Publish 3.0.0.5 MacOS

--- copy_bin_osx.sh
+++ copy_bin_osx.sh
@@ -4,6 +4,5 @@ cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalo
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/vmpc2000xl.vst3 osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/AU/vmpc2000xl.component osx/
 cd osx
-#zip -v -r vmpc2000xl.zip vmpc2000xl.app
 zip -v -r -m vmpc2000xl.component.zip vmpc2000xl.component
 zip -v -r -m vmpc2000xl.vst3.zip vmpc2000xl.vst3
