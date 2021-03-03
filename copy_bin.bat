b3791ea4a1d460e10724bc39baee4140a0b882d2
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-03 19:15:18 +0100

    Fix filesystem stuff for Windows

--- copy_bin.bat
+++ copy_bin.bat
@@ -1,9 +1,9 @@
-xcopy /F /Y "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\vmpc2000xl.exe" win32\
-xcopy /F /Y /E /I "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\vmpc2000xl.vst3" win32\vmpc2000xl.vst3
-xcopy /F /Y "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\vmpc2000xl.exe" win64\
-xcopy /F /Y /E /I "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\vmpc2000xl.vst3" win64\vmpc2000xl.vst3
+xcopy /F /Y "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\VMPC2000XL.exe" win32\
+xcopy /F /Y /E /I "..\vmpc-workspace-32\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\VMPC2000XL.vst3" win32\VMPC2000XL.vst3
+xcopy /F /Y "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\Standalone\VMPC2000XL.exe" win64\
+xcopy /F /Y /E /I "..\vmpc-workspace\vmpc-juce\build\vmpc2000xl_artefacts\Release\VST3\VMPC2000XL.vst3" win64\VMPC2000XL.vst3
 cd win32
-zip -v -r -m vmpc2000xl.vst3.zip vmpc2000xl.vst3
+zip -v -r -m VMPC2000XL.vst3.zip VMPC2000XL.vst3
 cd ../win64
-zip -v -r -m vmpc2000xl.vst3.zip vmpc2000xl.vst3
+zip -v -r -m VMPC2000XL.vst3.zip VMPC2000XL.vst3
 cd..
