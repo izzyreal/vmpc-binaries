5040e00b9bc6c456e8705004788695458ded5d0d
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-11 15:51:07 +0100

    One MacOS installer for standalone and plugins

--- copy_bin_osx.sh
+++ copy_bin_osx.sh
@@ -3,6 +3,3 @@ rm -rf osx/*
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/VMPC2000XL.app osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/VMPC2000XL.vst3 osx/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/AU/VMPC2000XL.component osx/
-cd osx
-zip -v -r -m VMPC2000XL-AU.zip VMPC2000XL.component
-zip -v -r -m VMPC2000XL-VST3.zip VMPC2000XL.vst3
