7009e539ecbe5dc4c658da3e3ff7758c71695b2e
Izmar Verhage <izmaelverhage@gmail.com>
2021-04-10 16:46:26 +0200

    Remove unneeded script

--- copy_bin_osx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf osx/*
-cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/VMPC2000XL.app osx/
-cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/VMPC2000XL.vst3 osx/
-cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/AU/VMPC2000XL.component osx/
