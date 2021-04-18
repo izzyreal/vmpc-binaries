eede8b6f90740f3269edd56dc725997d5526c5d4
Izmar Verhage <izmaelverhage@gmail.com>
2021-04-18 16:11:11 +0200

    Publish for Ubuntu 18 x64

--- copy_bin_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf linux/ubuntu1804/*
-cp -v ../vmpc-juce/Builds/LinuxMakefile/build/vmpc linux/ubuntu1804/ 
-cp -v ../vmpc-juce/Builds/LinuxMakefile/build/vmpc.so linux/ubuntu1804/ 
-chmod +x linux/ubuntu1804/vmpc
--- copy_bin_ubuntu18_x64.sh
+++ copy_bin_ubuntu18_x64.sh
@@ -2,7 +2,6 @@
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/LV2/VMPC2000XL.lv2 ./linux/ubuntu1804/
 cp -v -r ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/VST3/VMPC2000XL.vst3 ./linux/ubuntu1804/
 cp ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/VMPC2000XL ./linux/ubuntu1804/
-p ../vmpc-workspace/vmpc-juce/build/vmpc2000xl_artefacts/Release/Standalone/VMPC2000XL ./linux/ubuntu1804/
 cd linux/ubuntu1804
 zip -v -r -m VMPC2000XL-Ubuntu18-x64-LV2.zip VMPC2000XL.lv2
 zip -v -r -m VMPC2000XL-Ubuntu18-x64-VST3.zip VMPC2000XL.vst3
